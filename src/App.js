26d88a5f8b6742b3e23b33ba8c27bd3d67f93f83
satyadev0655 <103864602+satyadev0655@users.noreply.github.com>
2022-10-04 16:25:54 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './styles.css';
+import { Hello1 } from './components/Hello';
+import { Hello2 } from './components/Hello';
+export default function App() {
+	return (
+		<div className="App">
+			<Hello1 />
+			<Hello2 />
+		</div>
+	);
+}
